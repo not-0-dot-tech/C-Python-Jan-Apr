0703ff3f105fd5d813167ed94be4645246d71429
Alik Agarwala <alikagarwala@outlook.com>
2025-01-16 23:06:35 +0530

    2.c

---
+++ C/input/2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+
+    // input
+
+    printf("Enter a number: ");
+
+    scanf("%d", &n);
+
+    // output
+
+    printf("You entered: %d\n", n);
+
+    return 0;
+}

99f52e181e46f4ff30c52947fb8ac81ca9489248
Alik Agarwala <alikagarwala@outlook.com>
2025-01-16 23:21:28 +0530

    Add basic input/output program in 4.c

---
+++ C/input/4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+
+    // input
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+
+    // output
+
+    printf("%d %d %d\n", a, b, c);
+
+    return 0;
+}

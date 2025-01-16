308eaa267b371505807c6a986a26e6a3878269b3
Alik Agarwala <alikagarwala@outlook.com>
2025-01-16 23:45:29 +0530

    Add input handling and output display in 5.c

---
+++ C/input/5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+
+    // input
+
+    // scanf("%d", &a);
+    // scanf("%d", &b);
+    // scanf("%d", &c);
+
+    // scanf("%d %d %d", &a, &b, &c); // this line is equivalent to the above three lines
+    scanf("%d%d%d", &a, &b, &c); // this line is equivalent to the above line
+
+    // output
+
+    printf("%d %d %d\n", a, b, c);
+
+    return 0;
+}

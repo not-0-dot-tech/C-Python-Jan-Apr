dfa07806e16542fc0dbec029e0ec75ab73ecca35
Alik Agarwala <alikagarwala@outlook.com>
2025-01-18 22:30:35 +0530

    1.c  memory buffer

---
+++ C/input/memory-buffer/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+
+    // input
+
+    scanf("%d", &a); // read from memory buffer
+    scanf("%d", &b); // read from memory buffer
+
+    // output
+
+    printf("%d %d\n", a, b);
+
+    return 0;
+}

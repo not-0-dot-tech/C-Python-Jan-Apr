e93a0f705e6904778d5f0cf2dc4e526ae766c549
Alik Agarwala <alikagarwala@outlook.com>
2025-01-18 23:48:31 +0530

    Add input handling and output display in 4.c

---
+++ C/input/memory-buffer/4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char a, b;
+
+    // input
+
+    scanf("%c", &a); // read from memory buffer
+    scanf("%c", &b); // read from memory buffer
+
+    // output
+
+    printf("%c, %c\n", a, b);
+
+    return 0;
+}

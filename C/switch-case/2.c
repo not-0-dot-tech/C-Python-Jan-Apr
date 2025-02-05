b620511cefc7b565c6027825677061634ec7635a
Alik Agarwala <alikagarwala@outlook.com>
2025-02-05 22:29:01 +0530

    Add example demonstrating switch-case statements in C with multiple cases

---
+++ C/switch-case/2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    int input;
+    scanf("%d", &input);
+
+    switch (input)
+    {
+    case 4:
+        printf("Hi\n");
+        break;
+
+    case 3:
+        printf("Hello\n");
+
+    case 7:
+        printf("Bye\n");
+        break;
+
+    case 2:
+        printf("Hola\n");
+        break;
+
+    case 9:
+        printf("Lol\n");
+        break;
+
+    case 6:
+        printf("Yes\n");
+        break;
+
+    default:
+        printf("Sorry\n");
+    }
+
+    return 0;
+}

17ca16759b5d47999dac1ccccc88e56365a3294d
Alik Agarwala <alikagarwala@outlook.com>
2025-02-08 22:54:08 +0530

    2.c

---
+++ C/loop/while/2.c
@@ -0,0 +1,10 @@
+// infinite while loop
+
+#include <stdio.h>
+int main()
+{
+    while (1)
+        printf("Hello World\n"); // you can omit the curly braces if there is only one statement inside the loop
+
+    return 0;
+}

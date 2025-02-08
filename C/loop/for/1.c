0f83bf80e62adba0f03c01c4486a63197213a045
Alik Agarwala <alikagarwala@outlook.com>
2025-02-08 23:53:22 +0530

    1.c

---
+++ C/loop/for/1.c
@@ -0,0 +1,20 @@
+// for loop
+
+#include <stdio.h>
+int main()
+{
+    // print "Hello World" 5 times
+
+    int i;
+
+    for (i = 1; i <= 5; i++)
+    {
+        printf("Hello World\n");
+    }
+
+    printf("End of the loop\n");
+
+    printf("i = %d\n", i);
+
+    return 0;
+}

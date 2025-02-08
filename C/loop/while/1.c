a71caf0018c387c766f64cbcbb1700d8d619df66
Alik Agarwala <alikagarwala@outlook.com>
2025-02-08 22:49:36 +0530

    1.c

---
+++ C/loop/while/1.c
@@ -0,0 +1,19 @@
+// while loop
+
+#include <stdio.h>
+int main()
+{
+    int i = 1; // counter variable
+
+    while (i <= 5)
+    {
+        printf("Hello World\n");
+        i++;
+    }
+
+    printf("\n\n");
+
+    printf("%d\n", i); // 6
+
+    return 0;
+}

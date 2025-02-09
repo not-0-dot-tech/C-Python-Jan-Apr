e440365d5722386af94fedf719e01781d3128426
Alik Agarwala <alikagarwala@outlook.com>
2025-02-09 22:56:22 +0530

    Add example of continue statement in for-loop in C

---
+++ C/jump-statements/3.c
@@ -0,0 +1,17 @@
+// continue
+
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i == 5)
+            continue;
+
+        printf("%d ", i);
+    }
+
+    printf("\n");
+
+    return 0;
+}

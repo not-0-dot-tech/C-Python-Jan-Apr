817b58b31662915ca027dfa794af1804b09e4c94
Alik Agarwala <alikagarwala@outlook.com>
2025-02-25 23:33:05 +0530

    Add C program for input and output of a 2D array

---
+++ C/array/2D-array/input-output-2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int row, col;
+
+    scanf("%d%d", &row, &col);
+
+    int arr[row][col];
+
+    // input
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < col; j++)
+            scanf("%d", &arr[i][j]);
+
+    // output
+
+    printf("\n");
+    printf("\n");
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+            printf("%d ", arr[i][j]);
+
+        printf("\n");
+    }
+
+    return 0;
+}

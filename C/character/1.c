8d96ee69d5fd681eaa24c711e22879deb8edd74e
Alik Agarwala <alikagarwala@outlook.com>
2025-03-03 22:59:31 +0530

    1.c

---
+++ C/character/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char ch = 'A';
+    printf("The character is %c\n", ch);
+    printf("The ASCII value of the character is %d\n", ch);
+
+    return 0;
+}
+// The character is A
+// The ASCII value of the character is 65

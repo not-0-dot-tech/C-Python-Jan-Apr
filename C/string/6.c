e9c2c7587a50783f20edd6c21469d2a34887770e
Alik Agarwala <alikagarwala@outlook.com>
2025-03-04 00:03:29 +0530

    Add C program to demonstrate user input and output of a string

---
+++ C/string/6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+
+    //  Input string from user
+
+    scanf("%s", str);
+
+    //  Output string
+
+    printf("String: %s\n", str);
+
+    return 0;
+}

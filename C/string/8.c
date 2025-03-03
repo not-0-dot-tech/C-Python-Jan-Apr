6cb7076bf6f64f68edeea5220f8954cb44122045
Alik Agarwala <alikagarwala@outlook.com>
2025-03-04 00:17:54 +0530

    Add C program to demonstrate user input and output of a string using gets() and puts()

---
+++ C/string/8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    // Declare a string
+    char str[100];
+
+    // Input string from user using gets()
+    gets(str);
+
+    // Output string using puts()
+    puts(str);
+
+    return 0;
+}

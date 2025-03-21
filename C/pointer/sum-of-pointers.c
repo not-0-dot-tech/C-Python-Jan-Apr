2f4eba0ea448966304cda2a4f164d75140152b35
Alik Agarwala <alikagarwala@outlook.com>
2025-03-21 23:16:22 +0530

    Add C program demonstrating sum of pointer addresses

---
+++ C/pointer/sum-of-pointers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int arr[6] = {7, 1, 2, 9, 6, 4};
+
+    int *ptr1 = &arr[1];
+    int *ptr2 = &arr[4];
+
+    printf("ptr1 + ptr2: %d\n", ptr1 + ptr2);
+
+    return 0;
+}

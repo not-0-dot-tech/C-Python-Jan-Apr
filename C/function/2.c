c67ca2b53a628055e63b27427c3950c79d3ec186
Alik Agarwala <alikagarwala@outlook.com>
2025-03-23 22:42:04 +0530

    Add C program to print "Hello World" 10 times with function demonstration

---
+++ C/function/2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void printHello10Times(); // function prototype
+
+int main()
+{
+
+    printf("Hi...\n");
+    printf("Doing something...\n\n\n");
+
+    printHello10Times();
+
+    return 0;
+}
+
+void printHello10Times() // function definition
+{
+    // print Hello World 10 times
+    for (int i = 1; i <= 10; i++)
+        printf("Hello World - %d\n", i);
+
+    return; // return statement
+
+    printf("This line will not be executed\n"); // unreachable code
+}

cbb23e86b789b8b0f3350a873c1301fa18645a70
Alik Agarwala <alikagarwala@outlook.com>
2025-04-01 22:25:14 +0530

    1.c

---
+++ C/function/passing-array-in-function/1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void printArray(int *, int); // function prototype
+
+int main()
+{
+    int arr[5] = {9, 0, 5, 1, 3};
+    printArray(arr, 5); // function call by reference
+
+    return 0;
+}
+
+void printArray(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]); // print("%d ", *(arr + i));
+
+    printf("\n");
+}
